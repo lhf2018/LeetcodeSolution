d2c1da5aa5a1648f566ef3e10b84dfebc6ae3680
lhf2018 <1146953160@qq.com>
2020-01-27 20:14:53 +0800

    146

---
+++ src/shell/leetcode194.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Read from the file file.txt and print its transposed content to stdout.
+awk '{
+    for(i=1;i<=NF;i++){
+        if(NR==1){
+        res[i]=$i;
+        }else{
+        res[i]=res[i]" "$i
+        }
+    }
+}
+END{
+    for(i=1;i<=NF;i++){
+    print res[i]
+    }
+}' file.txt
